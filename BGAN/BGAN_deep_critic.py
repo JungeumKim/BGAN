83ca7735c273e5923f44664cf6a1783f42334570
JungeumKim <snukingdom@gmail.com>
2025-02-19 19:00:55 -0600

    new f_theta yuexi idea implemented

--- BGAN/BGAN_deep_critic.py
+++ BGAN/BGAN_deep_critic.py
@@ -1,17 +1,14 @@
 """
 This is code for  Adversarial Bayesian Simulation (Yuexi Wang, Veronika Rockova), developed and implemented by Jungeum Kim to adopt DeepSet with a regularizer.
 """
-
 from IPython.core.debugger import set_trace
-
 from BGAN import Critic as BGAN_CRITIC
 from BGAN import Generator as BGAN_GENERATOR
 from _nets.DeepSet_nets import Auto_ss
+import torch
 from BGAN import BGAN
 
-
 class Critic(BGAN_CRITIC):
-
 
     def __init__(self,theta_dim=2,x_dim=2,  f1_dim=2,f2_dim=2,
                  d_hidden = [128,128,128], leaky=0.1, aggregation=True,x_length=None, 
@@ -20,15 +17,11 @@ class Critic(BGAN_CRITIC):
                  ):
         super().__init__(input_dim = theta_dim,
                          cond_dim = f1_dim + f2_dim,
-                         d_hidden = d_hidden
-                         )
+                         d_hidden = d_hidden)
         self.ss = Auto_ss(f1_dim=f1_dim, f2_dim=f2_dim, x_dim=x_dim, aggregation=aggregation, x_length=x_length,
                                    factor=factor, f1_layers =f1_layers,
-                                   common_factor=common_factor,common_layers = common_layers
-                                   )
-
+                                   common_factor=common_factor,common_layers = common_layers)
     def forward(self, x, context):
-
         f_context = self.ss(context)# .unsqueeze(-1))
         return super().forward(x, f_context)
 
@@ -38,29 +31,21 @@ class Generator(BGAN_GENERATOR):
                 f1_dim=2,f2_dim=2, leaky=0.1,x_length=None, 
                  d_hidden = [128,128,128], aggregation=True,
                 factor=64, f1_layers =3,
-                 common_factor=64,common_layers = 3
-                 ):
+                 common_factor=64,common_layers = 3):
         
         super().__init__(d_hidden=d_hidden,
                          theta_dim=theta_dim,
                          z_dim = theta_dim,
                          cond_dim = f1_dim + f2_dim,
                          leaky=leaky)
-        
         self.d_cond = f1_dim + f2_dim
-        
         self.ss = Auto_ss(f1_dim=f1_dim, f2_dim=f2_dim, x_dim=x_dim, aggregation=aggregation, x_length=x_length,
                            factor=factor, f1_layers =f1_layers,
-                                   common_factor=common_factor,common_layers = common_layers
-                                    )
-
+                                   common_factor=common_factor,common_layers = common_layers)
 
     def forward(self, context, noise = None):
         f_context = self.ss(context)
         return super().forward(f_context, noise)
-
-
-
 
 class DBGAN(BGAN):
 
@@ -87,7 +72,6 @@ class DBGAN(BGAN):
                                    factor=factor, f1_layers =f1_layers,
                                    common_factor=common_factor,common_layers = common_layers
                                    )
-        
         self.critic = Critic(
                              theta_dim=theta_dim,
                              f1_dim=f1_dim,
@@ -100,3 +84,88 @@ class DBGAN(BGAN):
                              common_factor=common_factor,common_layers = common_layers)
         self.generator.to(device)        
         self.critic.to(device)
+
+#Pickup here
+
+class DBGAN_mix(DBGAN):
+
+    def __init__(self, simulator, theta_dim, x_dim, x_length,
+                 f1_dim=2, f2_dim=5,
+                 device="cuda", epoch=150, batch_size=200,
+                 seed=1234, d_hidden=128,
+                 critic_lr=0.001, generator_lr=0.001, aggregation=True,
+                 factor=64, f1_layers=3,
+                 common_factor=64, common_layers=3, hidden_layers=3,
+                 *args, **kwargs):
+        # Properly initialize the parent class (DBGAN)
+        super().__init__(simulator, theta_dim, x_dim, x_length,
+                         f1_dim=f1_dim, f2_dim=f2_dim, device=device, epoch=epoch,
+                         batch_size=batch_size, seed=seed, d_hidden=d_hidden,
+                         critic_lr=critic_lr, generator_lr=generator_lr,
+                         aggregation=aggregation, factor=factor, f1_layers=f1_layers,
+                         common_factor=common_factor, common_layers=common_layers,
+                         hidden_layers=hidden_layers, *args, **kwargs)
+        self.critic.ss =  Auto_ss(f1_dim=f1_dim, f2_dim=f2_dim,
+                                  x_dim=x_dim + theta_dim, #---> This is the only change
+                                  aggregation=aggregation, x_length=x_length,
+                                   factor=factor, f1_layers =f1_layers,
+                                   common_factor=common_factor,common_layers = common_layers)#
+        self.generator.ss =  Auto_ss(f1_dim=f1_dim, f2_dim=f2_dim,
+                                     x_dim=x_dim+theta_dim, #---> This is the only change
+                                     aggregation=aggregation, x_length=x_length,
+                                    factor=factor, f1_layers =f1_layers,
+                                   common_factor=common_factor,common_layers = common_layers)
+        # Override the forward methods
+        self.generator.forward = self.mixed_forward_gen.__get__(self.generator, Generator)
+        self.critic.forward = self.mixed_forward_critic.__get__(self.critic, Critic)
+
+    def mixed_forward_gen(self, context, noise=None):
+        if noise is None:
+            noise = torch.randn(context.size(0), self.generator.z_dim).to(context.device)
+
+        reshaped_noise = noise.unsqueeze(1).expand(context.shape[0], context.shape[1], self.generator.z_dim)
+        f_context = self.generator.ss(torch.cat([context, reshaped_noise], dim=-1))
+
+        return super(DBGAN.Generator, self.generator).forward(f_context, noise)
+
+    def mixed_forward_critic(self, x, context):
+        reshaped_x = x.unsqueeze(1).expand(context.shape[0], context.shape[1], -1)
+        f_context = self.critic.ss(torch.cat([context, reshaped_x], dim=-1))
+
+        return super(DBGAN.Critic, self.critic).forward(x, f_context)
+
+
+'''
+class DBGAN_mix(DBGAN):
+
+    def __init__(self, simulator, theta_dim,  x_dim, x_length,
+                 f1_dim=2, f2_dim=5,
+                 device="cuda",epoch=150, batch_size = 200,
+                 seed=1234, d_hidden = 128,
+                 critic_lr = 0.001, generator_lr = 0.001,aggregation=True,
+                 factor=64, f1_layers =3,
+                 common_factor=64,common_layers = 3,hidden_layers=3,
+                 *args, **kwargs):
+        super().__init__(xxx)
+
+        self.generator.forward = mixed_forward_gen
+        self.critic.forward = mixed_forward_critic
+        self.critic.ss = #
+        self.generator.ss = #
+
+def mixed_forward_gen(self, context, noise = None):
+    #f_context = self.ss(context)
+    if noise is None:
+        noise = torch.randn(context.size(0), self.d_noise).to(context.device)
+    reshaped_noise = noise.unsqueeze(1).expand(context.shape[0],context.shape[1], self.d_noise)
+    f_context = self.ss(torch.cat([context, reshaped_noise]))
+    return super().forward(f_context, noise)
+
+def mixed_forward_critic(self, x, context):
+    #x: theta - batch_size x d_theta
+    #context: data - batch_size x n x d  dimension
+    reshaped_x = x.unsqueeze(1).expand(context.shape[0],context.shape[1], -1)
+    f_context = self.ss(torch.cat([context, reshaped_x]))# .unsqueeze(-1))
+    #f_context = self.ss(context)# .unsqueeze(-1))
+    return super().forward(x, f_context)
+'''
