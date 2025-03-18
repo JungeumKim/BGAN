54d5dd0f27469b324a21159a228f6aa52bf08c7b
JungeumKim <snukingdom@gmail.com>
2025-03-18 09:48:02 -0500

    Trying to run the lotka voltera model

---
+++ exp_controller/Lotka_voltera/config.py
@@ -0,0 +1,43 @@
+import argparse
+import numpy as np
+import torch
+import random
+
+def parse_args(args, name_space):
+    parser = argparse.ArgumentParser(description='Arguments.')
+    parser.add_argument('--worktree',
+                        #default= "/home/kim2712/Desktop/research/BGAN/BGAN",
+                        #default= "/home/kim2712/Desktop/research/BGAN/BGAN/worktrees/bgan_4e83798/BGAN", : original exp
+                        default= "/home/kim2712/Desktop/research/BGAN/BGAN/worktrees/bgan_1839755/BGAN", #: after double deepset made
+                        help = "parent directory")
+    parser.add_argument('--exp_dir', default = "./",  help= 'global path')
+    parser.add_argument('--device', default = "cuda",  help= ' ')    
+    parser.add_argument('--method', default = "deepset",  help= ' ')
+    parser.add_argument('--random_repeat', type=int, default=200, help=' ')
+
+    parser.add_argument('--epoch', type=int, default = 200, help= ' ')
+    parser.add_argument('--n_iter', type=int, default = 100, help= ' ')
+    parser.add_argument('--batch_size', type=int, default = 128, help= ' ')
+
+    #parser.add_argument('--nu', type=int, default = 25, help= ' ')
+    #parser.add_argument('--sigma0_sq', type=float, default = 1)
+    #parser.add_argument('--mu0', type=float, default = 0)
+    #parser.add_argument('--kappa', type=int, default = 2, help= ' ')
+    
+    parser.add_argument('--seed', type=int, default = 12345, help= ' ')
+    parser.add_argument('--lr', type=float, default = 0.0001, help= ' ')
+    parser.add_argument('--n_steps', type=int, default=200, help=' ')
+
+    parser.add_argument('--factor', type=int, default = 16, help= ' ')
+    parser.add_argument('--f1_layers', type=int, default = 1, help= ' ')
+    parser.add_argument('--common_layers', type=int, default = 32, help= ' ')
+    parser.add_argument('--common_factor', type=int, default = 2, help= ' ')    
+    parser.add_argument('--hidden_layers', type=int, default = 1, help= ' ')
+    parser.add_argument('--d_hidden', type=int, default = 16, help= ' ')
+    
+
+    #learning setting:
+    args = parser.parse_args(args, namespace=name_space) #parser.parse_args()
+    return args
+
+
